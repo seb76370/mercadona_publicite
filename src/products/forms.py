bfd77f1e0c625fb174d9696abaebdc0ebc0cb5a1
sebastien baudet <seb.baudet76@gmail.com>
2023-05-09 21:22:40 +0200

    ajout de la methode add_product

--- src/products/forms.py
+++ src/products/forms.py
@@ -5,4 +5,10 @@ from .models import Categories, Produits
 class CategoriesForm(forms.ModelForm):
     class Meta:     
         model = Categories
-        fields = ('Libelle',)        
+        fields = ('Libelle',)   
+
+
+class ProduitForm(forms.ModelForm):
+    class Meta:     
+        model = Produits
+        fields = ('libelle','description','prix','images','categorie')
--- src/products/views.py
+++ src/products/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.list import ListView
 from .models import Categories
-from products.forms import CategoriesForm
+from products.forms import CategoriesForm, ProduitForm
 
 @csrf_exempt
 def add_cat(request):
@@ -20,8 +20,25 @@ def add_cat(request):
           errors = form.errors.as_data()
           errors_text = str(errors['Libelle'][0])
           return JsonResponse({"message":"fail","error":errors_text})
+    
+@csrf_exempt
+def add_product(request):
+    if request.method != "POST":
+         return HttpResponse("Erreur de requete")
+    
+    form = ProduitForm(request.POST, request.FILES)
+    if form.is_valid():
+          form.save() 
+          return JsonResponse({"message":"success"})
+    else:
+          errors = form.errors.as_data()
+          print(errors)
+          # errors_text = str(errors['Libelle'][0])
+          return JsonResponse({"message":"fail","error":"eee"})
 
 class list_categorie(ListView):
      model = Categories
      context_object_name = 'list_cat'
      template_name = 'testlist.html'
+
+
