d9045f5824b0d1be9143d9351b6b974fba63b5f6
sebastien baudet <seb.baudet76@gmail.com>
2023-05-09 21:04:50 +0200

    update

--- src/products/forms.py
+++ src/products/forms.py
@@ -1,8 +1,8 @@
 
 from django import forms
-from .models import Categories
+from .models import Categories, Produits
 
 class CategoriesForm(forms.ModelForm):
     class Meta:     
         model = Categories
-        fields = ('Libelle',)
+        fields = ('Libelle',)        
